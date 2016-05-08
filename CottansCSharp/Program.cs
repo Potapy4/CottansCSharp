d1b51ae2b6c3cdd7d939d7e007b150ddd2341e1e
Nikita <dev.potapy4@hotmail.com>
2016-05-08 18:16:35 +0300

    This fixed #4 and fixed #5

--- CottansCSharp/Program.cs
+++ CottansCSharp/Program.cs
@@ -42,44 +42,42 @@ namespace CottansCSharp
             }
             return sum;
         }
-        
+
         static string GetCreditCardVendor(string number)
         {
-            RemoveDashes(ref number);
-            string first4digit = number.Substring(0, 4);
-            int[] cardNumber = new int[first4digit.Length];
             string vendor = "Unknown";
-
-            for (int i = 0; i < cardNumber.Length; ++i)
-                cardNumber[i] = number[i] - '0';
-
-            int tmp = cardNumber[0] * 10 + cardNumber[1]; // Получаем первые 2 цифры и записываем в переменную
-            // Алгоритм: Указываем диапазон (начало, ДО какой позиции проверить) и проверяем (если наше значение и длина номера карты подходит под условие, то назначаем вендор)
-            if (Enumerable.Range(34, 4).Contains(tmp) && number.Length == 15)
-                vendor = "American Express";
-            else if (Enumerable.Range(50, 1).Contains(tmp) || Enumerable.Range(56, 14).Contains(tmp) && number.Length >= 12 && number.Length <= 19)
-                vendor = "Maestro";
-            else if (Enumerable.Range(51, 5).Contains(tmp) && number.Length >= 16 && number.Length <= 19)
-                vendor = "Master Card";
-            else if (tmp / 10 == 4 && number.Length >= 13) // Для VISA
-                vendor = "VISA";
-            else if (tmp == 35 && number.Length >= 16) // Если начинается на 35 и длина от 16, то возможно это JCB, дальше чекнем
+            if (IsCreditCardNumberValid(number))
             {
-                tmp = cardNumber[0] * 1000 + cardNumber[1] * 100 + cardNumber[2] * 10 + cardNumber[3];
-                if (tmp >= 3528 && tmp <= 3589)
-                    vendor = "JCB";
+                string first4digit = number.Substring(0, 4);
+                int[] cardNumber = new int[first4digit.Length];
+
+                for (int i = 0; i < cardNumber.Length; ++i)
+                    cardNumber[i] = number[i] - '0';
+
+                int tmp = cardNumber[0] * 10 + cardNumber[1]; // Получаем первые 2 цифры и записываем в переменную
+                                                              // Алгоритм: Указываем диапазон (начало, ДО какой позиции проверить) и проверяем (если наше значение и длина номера карты подходит под условие, то назначаем вендор)
+                if (Enumerable.Range(34, 4).Contains(tmp) && number.Length == 15)
+                    vendor = "American Express";
+                else if (Enumerable.Range(50, 1).Contains(tmp) || Enumerable.Range(56, 14).Contains(tmp) && number.Length >= 12 && number.Length <= 19)
+                    vendor = "Maestro";
+                else if (Enumerable.Range(51, 5).Contains(tmp) && number.Length >= 16 && number.Length <= 19)
+                    vendor = "Master Card";
+                else if (tmp / 10 == 4 && number.Length >= 13) // Для VISA
+                    vendor = "VISA";
+                else if (tmp == 35 && number.Length >= 16) // Если начинается на 35 и длина от 16, то возможно это JCB, дальше чекнем
+                {
+                    tmp = cardNumber[0] * 1000 + cardNumber[1] * 100 + cardNumber[2] * 10 + cardNumber[3];
+                    if (tmp >= 3528 && tmp <= 3589)
+                        vendor = "JCB";
+                }
             }
-
-            // P.S. -  Есть карты разной длины и типа (Бизнес, Дебетовая, Кредитная и т.п) - т.е. может быть и VISA с длиной < 16 и MasterCard > 16.
-            // Инфа взята с http://www.freeformatter.com/credit-card-number-generator-validator.html
-
             return vendor;
+
         }
 
         static bool IsCreditCardNumberValid(string number)
         {
             RemoveDashes(ref number);
-
             int[] cardNumber = new int[number.Length];
 
             for (int i = 0; i < cardNumber.Length; ++i)
@@ -92,25 +90,34 @@ namespace CottansCSharp
 
         static string GenerateNextCreditCardNumber(string number)
         {
-            RemoveDashes(ref number);            
-            long nextCC = Int64.Parse(number);
-            nextCC++;
+            RemoveDashes(ref number);
+            string originalVendor = GetCreditCardVendor(number);
+            string newVendor = "No more CC numbers available for this vendor or invalid CC number";
+            if (originalVendor != "Unknown")
+            {
+                long nextCC = Int64.Parse(number);
 
-            while (!IsCreditCardNumberValid(Convert.ToString(nextCC)))
-                nextCC++;
-            return Convert.ToString(nextCC);
+                while (!IsCreditCardNumberValid(Convert.ToString(++nextCC)))
+                    nextCC++;
+
+                if (originalVendor == GetCreditCardVendor(Convert.ToString(nextCC)))
+                    newVendor = Convert.ToString(nextCC);
+            }
+
+            return newVendor;
         }
 
         static void Main(string[] args)
         {
-            string s = GetCreditCardVendor("4917610000000000003");
+            string s = GetCreditCardVendor("4999999999999999993");
             Console.WriteLine(s);
 
-            Console.WriteLine(IsCreditCardNumberValid("4917610000000000003 "));
+            Console.WriteLine(IsCreditCardNumberValid("4999999999999999993"));
 
-            string a = GenerateNextCreditCardNumber("4917610000000000003    ");
+            string a = GenerateNextCreditCardNumber("4999999999999999993");
             Console.WriteLine(a);
-          
+
+            Console.WriteLine(GetCreditCardVendor(a));
 
         }
     }
